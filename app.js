b9f9450e39bd79337c27555b1a19495b08006fe8
nick1600 <77914421+Nick1600@users.noreply.github.com>
2021-07-21 21:20:35 +1200

    first commit, complete

---
+++ app.js
@@ -0,0 +1,29 @@
+const celsiusToFahrenheit = (celsius) => {
+    return celsius * (9/5) + 32;
+}
+const fahrenheitToCelsius = (fahrenheit) => {
+    return (fahrenheit - 32) * (5/9);
+}
+
+const tempInput = process.argv[2];
+const unit = process.argv[3].toLowerCase();
+
+const convert = (temperature, unitInput) => {
+
+
+    
+    const tempFahrenheitResult = celsiusToFahrenheit(tempInput);
+    const tempCelsiusResult = fahrenheitToCelsius(tempInput);
+
+    if (unitInput === 'c' || unitInput === 'celsius') {
+        console.log(`${temperature} degrees Celsius is ${tempFahrenheitResult} degrees Fahrenheit`);
+    } else if (unitInput === 'f' || unitInput === 'fahrenheit') {
+        console.log(`${temperature} degrees Fahrenheit is ${tempCelsiusResult} degrees Celsius`);
+    } else {
+        console.log('Please us syntax: temperature(number) unit(celsius or fahrenheit)');
+    }
+}
+
+convert(tempInput, unit);
+
+
